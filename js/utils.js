39c84ce7b557a9756bbeb758d856aea4b983c9b8
154852 <louisemileploix@icloud.com>
2018-12-11 16:21:11 +0000

    Added all.html page

--- js/utils.js
+++ js/utils.js
@@ -41,7 +41,7 @@ Utils.DataBaseConnection = class {
         if (window.Fuse) {
             this.fuse = new Fuse(this.data, {
                 shouldSort: true,
-                threshold: 0.6,
+                threshold: 0.4,
                 location: 0,
                 distance: 100,
                 maxPatternLength: 32,
