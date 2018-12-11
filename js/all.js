e1a0d55f473925409e2c004fb45740b88d9d95d8
154852 <louisemileploix@icloud.com>
2018-12-11 16:47:04 +0000

    Publishing

--- js/all.js
+++ js/all.js
@@ -1,16 +1,7 @@
 if (Utils.mobileAndTabletcheck()) new Background(document.querySelector('#bg'));
-
-// alert('ok')
-
-// const search = document.querySelector('#search');
-// const main = document.querySelector('#main');
 const searchResults = document.querySelector('#search-results');
-console.log(searchResults)
 
 const connection = new Utils.DataBaseConnection();
 connection.promise().then(() => {
-    console.log(connection.data)
     Utils.DataBaseConnection.generateSearchResults(searchResults, connection.data);
-
-    console.log(connection.data)
 })
